b0b62d6efe336a2a125df37457c9ed7c20fc3e05
Travis Thieman <travis.thieman@gmail.com>
2019-05-30 11:20:07 -0400

    Get MyPy types to actually be detected when installed (#38)

--- setup.py
+++ setup.py
@@ -44,5 +44,6 @@ setuptools.setup(
     ],
     keywords='token access authorization',
     packages=setuptools.find_packages(),
+    package_data={"mbq.client": ["py.typed"]},
     zip_safe=False,
 )
